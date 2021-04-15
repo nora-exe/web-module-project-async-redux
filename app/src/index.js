17c210b4e5cb13ab4e52e2a6b0d05d07a11e3049
Nora C <nocorser@gmail.com>
2021-04-15 01:07:34 -0500

    mvp, some styling

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,26 @@
+//* 💡 Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+//* 💡 Redux
+import { reducer } from './store/reducers';
+
+//* 💡 Components
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//* 💡 Styling
+import './index.css';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
